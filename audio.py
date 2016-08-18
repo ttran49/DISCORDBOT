37f5b75395e7161cd194fa383f06aa5ab8cfd3d7
Chuck Tran <ttran49@masonlive.gmu.edu>
2016-08-18 12:57:08 -0400

    "Finished" play() audio- need test

--- audio.py
+++ audio.py
@@ -16,9 +16,9 @@ class Audio
 	#creating an instance of playlist
 	playlist= Playlist();
 	
-	def __init__(self, voice_channel):
+	def __init__(self, voice_channel, client_object):
 		self.voice_channel = voice_channel
-	
+		self.client_object = client_object
 	
 	def add_song_to_playlist():
 		song_url=input("Please enter a song URL: ")
@@ -32,4 +32,15 @@ class Audio
 	def play():
 		if (playlist.size() == 0):
 			raise EmptyPlaylist()
-		
+		#play the music
+		voice = client_object.join_voice_channel(voice_channel)
+		for i in (playlist.size()-1):
+			player = voice.create_ytdl_player(playlist.currentsong())
+			await player.start()
+			playlist.nextsong()
+	
+	def skipsong():
+		playlist.removethsong(0)
+		play()
+	
+	
