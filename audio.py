3ed5ea0d1cb3f0d94bb8dc7a27ef620d2f7d84c0
Chuck Tran <ttran49@masonlive.gmu.edu>
2016-08-19 14:47:16 -0400

    "Finished"- need test

--- audio.py
+++ audio.py
@@ -18,19 +18,22 @@ class Audio
 	__playlist= Playlist();
 	__is_playing=False
 	__Pausing==False
+	__player=None
 	
 	#Constructor, get the client or bot object from bot.py
 	def __init__(self, voice_channel, client_object):
 		self.voice_channel = voice_channel
 		self.client_object = client_object
 	
-	#adding a new song to playlist
+	###################################################################
 	#
+	#	Adding a new song to playlist
 	#	Expected exception:
 	#		Playlist.DuplicateSongURL - Song/URL already exists
 	#		Playlist.NotValidNameOrURL - Song/URL not in valid format aka String
-	def add_song_to_playlist():
-		song_url=input("Please enter a song URL: ")
+	#
+	######################################################################3
+	def add_song_to_playlist(song_url):
 		try:
 			playlist.addsong(song_url)
 		except Playlist.DuplicateSongURL:
@@ -38,47 +41,90 @@ class Audio
 		except Playlist.NotValidNameOrURL:
 			print("Not a valid input, must be a string of a song url")
 	
+	##########################################################
+	#
+	#	Create a stream for play(). 
+	#	Return: None if the URL is not from youtube or soundcloud
+	#	TODO: create one for soundcloud 
+	#
+	##########################################################
 	def __create_a_stream(playlist):
 		voice = client_object.join_voice_channel(voice_channel)
 		player=None
+		
+		#YOUTUBE
 		if "youtube" in playlist.currentsong():
 			player= voice.create_ytdl_player(playlist.currentsong())
+		#SOUNDCLOUD
 		elif "soundcloud" in playlist.currentsong():
 			#player=
 		return player
-		
-	#Take the bot object create a stream to play music from youtube
-	# TODO: create one for soundcloud
+	
+	#########################################################
+	#
+	#	Start to play songs
+	#
+	#########################################################
 	def play():
 		if (playlist.size() == 0):
 			raise EmptyPlaylist()
 		#play the music
 		for i in (playlist.size()-1):
-			player = __create_a_stream(__playlist)
-			player.start()
+			__player = __create_a_stream(__playlist)
+			__player.start()
 			__is_playing=True
 			if player.is_done():
 				__playlist.nextsong()
 		__is_playing=False
 		
-	# TODO: figure out how to implement pause and resume with the current
-	# variable scope
+	########################################################
+	#
+	#	Pause the current song if is playing
+	#
+	########################################################
 	def pause():
-		if not __is_playing:
+		if not __is_playing and not (__player is None):
 			__Pausing=True
-			
+			__player.pause()
 		else:
 			print("No song is currently playing")
-		
+	
+	###################################################
+	#
+	#	Resume the song if paused or was playing	
+	#
+	###################################################
 	def resume():
-		if not __Pausing:
+		if not __Pausingand not (__player is None):
 			__Pausing=False
-			
+			__player.resume()
 		else:
 			print("Song is already paused")
+			
+	###################################################
+	#
+	#	Set the volume, must run play() once before run this
+	#	1.0 = 100%, 2.0 = 200%. Default = 1.0/100%
+	#	if pass in number which is negative or >2 => set to default volume
+	#
+	###################################################
+	def set_volume(number):
+		if __player is None:
+			print ("Player does not exist, call play() before call set_volume")
+			return None
+		if number.is_interger():
+			if number > 0 and number <= 2:
+				__player.volume(float(number))
+				return None
+			else:
+				__player.volume(1.0)
+				return None
 	
+	####################################################
+	#
+	#	Skip the current song 
+	#
+	####################################################
 	def skipsong():
 		__playlist.removethsong(0)
 		play()
-	
-	
