39857878532c2ed86e9f0e74e9e79e3c837391c2
Chuck Tran <ttran49@masonlive.gmu.edu>
2016-08-19 13:32:24 -0400

    Add files via upload

--- audio.py
+++ audio.py
@@ -15,7 +15,9 @@ class Audio
 	###############################################
 	
 	#creating an instance of playlist
-	playlist= Playlist();
+	__playlist= Playlist();
+	__is_playing=False
+	__Pausing==False
 	
 	#Constructor, get the client or bot object from bot.py
 	def __init__(self, voice_channel, client_object):
@@ -36,13 +38,13 @@ class Audio
 		except Playlist.NotValidNameOrURL:
 			print("Not a valid input, must be a string of a song url")
 	
-	def _create_a_stream(playlist):
+	def __create_a_stream(playlist):
 		voice = client_object.join_voice_channel(voice_channel)
 		player=None
 		if "youtube" in playlist.currentsong():
 			player= voice.create_ytdl_player(playlist.currentsong())
 		elif "soundcloud" in playlist.currentsong():
-			player=
+			#player=
 		return player
 		
 	#Take the bot object create a stream to play music from youtube
@@ -52,18 +54,31 @@ class Audio
 			raise EmptyPlaylist()
 		#play the music
 		for i in (playlist.size()-1):
-			player = _create_a_stream(playlist)
+			player = __create_a_stream(__playlist)
 			player.start()
+			__is_playing=True
 			if player.is_done():
-				playlist.nextsong()
-	def pause():
+				__playlist.nextsong()
+		__is_playing=False
 		
+	# TODO: figure out how to implement pause and resume with the current
+	# variable scope
+	def pause():
+		if !__is_playing:
+			__Pausing=True
+			
+		else:
+			print("No song is currently playing")
 		
 	def resume():
-		
+		if !__Pausing:
+			__Pausing=False
+			
+		else:
+			print("Song is already paused")
 	
 	def skipsong():
-		playlist.removethsong(0)
+		__playlist.removethsong(0)
 		play()
 	
 	
